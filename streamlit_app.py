5ee77f9cb846530db3270f4256d347a36af39303
Luuk Rutten <luuk.rutten@cloudnation.nl>
2024-11-26 13:11:44 +0100

    fix a

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ def generate_sankeys():
 
         fig = go.Figure(
             go.Sankey(
-                textfont=dict(color=f"rgb(0,0,0,{st.session_state.opacity})", size=1),
+                textfont=dict(color=f"rgba(0,0,0,{st.session_state.opacity})", size=0),
                 node={
                     "label": nodes.index,
                     "color": [
